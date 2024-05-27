a503113e7fd34b7d50a5fa4bdf7b7abcdc4a9877
Yurii Surzhykov <yuriisurzhykov@gmail.com>
2024-05-26 19:07:53 -0700

    Fix gradle settings for java version based on project properties
    Signed-off-by: Yurii Surzhykov <yuriisurzhykov@gmail.com>

--- location-data/repository/build.gradle.kts
+++ location-data/repository/build.gradle.kts
@@ -5,10 +5,10 @@ plugins {
 
 android {
     namespace = "com.github.yuriisurzhykov.purs.data.repository"
-    compileSdk = 34
+    compileSdk = ProjectProperties.compileSdkVersion
 
     defaultConfig {
-        minSdk = 24
+        minSdk = ProjectProperties.minSdkVersion
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -24,11 +24,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = ProjectProperties.javaSourceCompatibility
+        targetCompatibility = ProjectProperties.javaTargetCompatibility
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = ProjectProperties.kotlinJvmTarget
     }
 }
 
